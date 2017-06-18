b4cbc2426b6c6ee705e4603d627408d3a8bea338
Minh Nguyen <nguyen_ba_minh@yahoo.com>
2017-06-18 09:33:40 -0700

    added role and seed db with users

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,8 +4,12 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable, :secure_validatable
 
-  validates :name,  presence: true, length: { in: 1..50 }
-  validates :email,  presence: true, uniqueness: true, length: { in: 5..150 },
-            :format => { :with => /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\Z/ }
-  validates :password,  presence: true, length: { in: 8..128 }
+  #validates :name,  presence: true, length: { in: 1..50 }
+  #validates :email,  presence: true, uniqueness: true, length: { in: 5..150 },
+  #          :format => { :with => /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\Z/ }
+  #validates :password,  presence: true, length: { in: 8..128 }
+
+  enum role: [:standard, :premium, :admin]
+	# before_save { self.role ||= :standard }  already set at db level
+
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,44 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+User.delete_all
+
+# create standard users
+6.times do |n|
+   name  = Faker::Name.name
+   email = "standard#{n+1}@yahoo.com"
+   password = "Password1"
+   User.create!(name:  name,
+                email: email,
+                password:              password,
+                password_confirmation: password,
+                role: 'standard' )
+end
+
+# Create premium users
+6.times do |n|
+   name  = Faker::Name.name
+   email = "premium#{n+1}@yahoo.com"
+   password = "Password1"
+   User.create!(name:  name,
+                email: email,
+                password:              password,
+                password_confirmation: password,
+                role: 'premium' )
+end
+
+# Create Admin users
+name  = "Admin User"
+email = "admin@yahoo.com"
+password = "Password1"
+User.create!(name:  name,
+            email: email,
+            password:              password,
+            password_confirmation: password,
+            role: 'admin' )
+
+puts "Seed finished"
+puts "#{User.count} users created"
+puts "-----  #{User.where( role: 'standard').count}  STANDARD"
+puts "-----  #{User.where( role: 'premium').count}  PREMIUM"
+puts "-----  #{User.where( role: 'admin').count}  ADMIN"
